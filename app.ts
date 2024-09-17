702ef77325dc0e2f12bcc9b318ed6872b1f0af51
Alveena Aftab <162828631+AlveenaAftab@users.noreply.github.com>
2024-09-17 14:38:19 -0700

    Teacher_Management_System

--- app.ts
+++ app.ts
@@ -1,192 +1,86 @@
-#! /usr/bin/env node
-
+#!/usr/bin/env node
 import inquirer from "inquirer";
-import chalk from "chalk";
 
+let randomNumber: number = Math.floor(100000 + Math.random() * 80000);
 
-console.log(chalk.red("\n\t\tTypescript Quiz Test\t\n"));
-
-const quiz :{
-question_1 : string
-question_2: string
-question_3: string
-question_4 : string
-question_5: string
-question_6 : string
-question_7 : string
-question_8 : string
-question_9 : string
-question_10 : string
-} = await inquirer.prompt([
+let teacherDetails=await inquirer.prompt([
     {
-        name:"question_1",
-        type : "list",
-        message : chalk.blue("\n\t Q1.What is Typescript primarily used for?? \t\n"),
-        choices :["Memory Management" , "Dynamic Typing" ,"Static Typing", "none of these"]
+        name: "name",
+        type:"input",
+        message:"Please Enter teacher's name",
+        validate: function(value){
+            if(/^[A-Za-z\s]+$/.test(value)){
+                return true;
+            }
+            return "Please enter a valid name (only alphabets are allowed).";
+        }
+    },
+    {
+        name: "course",
+        type: "list",
+        message: "Select the course:",
+        choices: ["Html", "Css", "Javascript", "Typescript", "Nextjs"],
+   },
+    {
+         name:"experience",
+         type:"input",
+         message:"Experience in years?",
+         validate: function (value) {
+            if (value.trim()=="" || isNaN(Number(value)) || Number(value) <= 0) {
+              return "Please enter a valid number of years.";
+            }
+            return true;
+        },
+    },
+    {
+         name:"contact",
+         type:"input",
+         message:"Please Enter your phone Number",
+         validate: function (value) {
+            if (!value.trim() || !/^\d{11}$/.test(value)) {
+              return "Please enter a valid 11-digit contact number.";
+            }
+            return true;}
+    },
+    {
+         name: "email",
+         type: "input",
+         message: "Please enter your email address:",
+         validate: function (value) {
+         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+         if (!value.trim() || !emailPattern.test(value)) {
+          return "Please enter a valid email address.";
+         }
+         return true;}
+    },
+    
+    {
+    
+            name: "classschedule",
+            type: "list",
+            message: "Please enter the class schedule (e.g., Mon 10-12, Wed 2-4):",
+            choices:["Mon 9am-12am","Mon 2pm-5pm","Mon 7pm-10pm","Tue 9am-12am","Tue 2pm-5pm","Tue 7pm-10pm",
+                "Wed 9am-12am","Wed 2pm-5pm","Wed 7pm-10pm"]
         
-},
-
-{
-    name:"question_2",
-    type : "list",
-    message : chalk.blue("\n\t Q2.What are the three main `simple types` in typeScript?? \t\n"),
-    choices :["Boolean,Number,String" , "Array,Object,Boolean" , "String,number,array"]
-},
-{
-    name:"question_3",
-    type : "list",
-    message : chalk.blue("\n\t Q3.Which type of data is written 10 in inverted commas?? \t\n"),
-    choices :["string" , "boolean" , "number"]
-},
-{
-    name:"question_4",
-    type : "list",
-    message : chalk.blue("\n\t Q4.What are arrays in typeScript?? \t\n"),
-    choices :["container to store multiple data" , "container to store a single data" , "none of these"]
-},
-{
-    name:"question_5",
-    type : "list",
-    message : chalk.blue("\n\t Q5.shift method is used for what purpose.....?? \t\n"),
-    choices :["remove last element from array" , "remove element from the start of an array" , "replace element"]
-},
-{
-    name:"question_6",
-    type : "list",
-    message : chalk.blue("\n\t Q6.Which command is valid to make pakage.json file?? \t\n"),
-    choices :["tsc init -y" , "npm --init" , "npm init -y"]
-},
-{
-    name:"question_7",
-    type : "list",
-    message :chalk.blue( "\n\t Q7.inquirers are used for ?? \t\n"),
-    choices :["make array" , "take input from a user" , "make a file"]
-},
-{
-    name:"question_8",
-    type : "list",
-    message : chalk.blue("\n\t Q8.tsc --init is used for make a file of ?? \t\n"),
-    choices :["typescript" , "ts config" , "javascript"]
-},
-{
-    name:"question_9",
-    type : "list",
-    message : chalk.blue("\n\t Q9.In Typescript, which symbol is commonly used to terminate a statement?? \t\n"),
-    choices :[".",":",";",","]
-},
-{
-    name:"question_10",
-    type : "list",
-    message : chalk.blue("\n\t Q10.Which method of Inquirer.js is used to start the prompt interface and receive user input?? \t\n"),
-    choices :["start()","prompt()","init()","run()"]
-}
+    },
+    {
+            name:"select",
+            type:"list",
+            message:"What would you like to do next?",
+            choices:["view status","exit"]
+        }
 ])
-
-let score : number = 0;
-const total_score:number = 10;
-
-switch(quiz.question_1){
-    case "Static Typing" :
-    console.log(chalk.green("\n\tQuestion number 1 is correct\t\n"));
+      if(teacherDetails.select==="view status"){
+        console.log(`\n******Status****\n`)
+        console.log(`Teacher Name:${teacherDetails.name}`);
+        console.log(`Teacher ID: ${randomNumber}`);
+        console.log(`Phone Number: ${teacherDetails.contact}`);
+        console.log(`Email Address: ${teacherDetails.email}`);
+        console.log(`Experience in years: ${teacherDetails.experience}`)
+        console.log(`Slot: ${teacherDetails.classschedule}`)
+        console.log(`Course: ${teacherDetails.course}`);
+      } else {
+        console.log(`\nExititng Student Management System`)
+        }
     
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 1 is incorrect"));
-}
-switch(quiz.question_2){
-    case "Boolean,Number,String" :
-    console.log(chalk.green("\n\tQuestion number 2 is correct\t\n"));
     
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 2 is incorrect"));
-    
-}
-switch(quiz.question_3){
-    case "string" :
-    console.log(chalk.green("\n\tQuestion number 3 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 3 is incorrect"));
-    
-}
-switch(quiz.question_4){
-    case "container to store multiple data" :
-    console.log(chalk.green("\n\tQuestion number 4 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 4 is incorrect"));
-    
-}
-switch(quiz.question_5){
-    case "remove element from the start of an array" :
-    console.log(chalk.green("\n\tQuestion number 5 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 5 is incorrect"));
-    
-}
-switch(quiz.question_6){
-    case "npm init -y" :
-    console.log(chalk.green("\n\tQuestion number 6 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 6 is incorrect"));
-    
-}
-switch(quiz.question_7){
-    case "take input from a user" :
-    console.log(chalk.green("\n\tQuestion number 7 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 7 is incorrect"));
-    
-}
-switch(quiz.question_8){
-    case "ts config" :
-    console.log(chalk.green("\n\tQuestion number 8 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 8 is incorrect"));
-    
-}
-switch(quiz.question_9){
-    case ";" :
-    console.log(chalk.green("\n\tQuestion number 9 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 9 is incorrect"));
-    
-}
-switch(quiz.question_10){
-    case "prompt()":
-    console.log(chalk.green("\n\tQuestion number 10 is correct\t\n"));
-    
-    ++score;
-    break;
-    default:
-    console.log(chalk.red("Question number 10 is incorrect"));
-    
-}
-if(score == total_score){
-console.log(chalk.yellow(`Congratulations Your Score is : ${score}/${total_score}`));
-}else{
-    console.log(chalk.yellow(`\nYour Score is : ${score}/${total_score}`));
-    
-}
